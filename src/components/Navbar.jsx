eb6cdb60a07ba88b463413806f3d24bd18c2abe1
furkanevin <furkanevin00@gmail.com>
2023-01-27 03:03:46 +0300

    Exchanges added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import {
   HomeOutlined,
@@ -11,6 +11,23 @@ import { Link } from 'react-router-dom';
 import icon from '../images/logo.png';
 import MenuItem from 'antd/es/menu/MenuItem';
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -18,21 +35,29 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptonite</Link>
         </Typography.Title>
+        <Button
+          className="menu-control-container"
+          onClick={() => setActiveMenu(!activeMenu)}
+        >
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme="dark">
-        <MenuItem icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
-        </MenuItem>
-        <MenuItem icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-        </MenuItem>
-        <MenuItem icon={<MoneyCollectOutlined />}>
-          <Link to="/exchanges">Home</Link>
-        </MenuItem>
-        <MenuItem icon={<BulbOutlined />}>
-          <Link to="/news">News</Link>
-        </MenuItem>
-      </Menu>
+      {activeMenu && (
+        <Menu theme="dark">
+          <MenuItem icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </MenuItem>
+          <MenuItem icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </MenuItem>
+          <MenuItem icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </MenuItem>
+          <MenuItem icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </MenuItem>
+        </Menu>
+      )}
     </div>
   );
 };
--- src/pages/Exchanges.jsx
+++ src/pages/Exchanges.jsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import millify from 'millify';
+import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+import HTMLReactParser from 'html-react-parser';
+import useApi from './../services/cryptoApi';
+
+const { Text } = Typography;
+const { Panel } = Collapse;
 
 const Exchanges = () => {
-  return <div>Exchanges</div>;
+  const api = useApi();
+  const [exchanges, setExchanges] = useState(null);
+
+  useEffect(() => {
+    api
+      .get('https://coinranking1.p.rapidapi.com/exchange/-zdvbieRdZ/coins')
+      .then((res) => setExchanges(res.data?.data?.coins))
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (!exchanges) return 'loading';
+
+  return (
+    <>
+      <Row style={{ marginBottom: '20px' }}>
+        <Col span={6}>Exchanges</Col>
+        <Col span={6}>24h Trade Volume</Col>
+        <Col span={6}>Markets</Col>
+        <Col span={6}>Price</Col>
+      </Row>
+      <Row>
+        {exchanges.map((exchange) => (
+          <Col span={24}>
+            <Collapse>
+              <Panel
+                key={exchange.id}
+                showArrow={false}
+                header={
+                  <Row key={exchange.id}>
+                    <Col span={6}>
+                      <Text>
+                        <strong>{exchange.rank}.</strong>
+                      </Text>
+                      <Avatar
+                        className="exchange-image"
+                        src={exchange.iconUrl}
+                      />
+                      <Text>
+                        <strong>{exchange.name}</strong>
+                      </Text>
+                    </Col>
+                    <Col span={6}>${millify(exchange['24hVolume'])}</Col>
+                    <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+                    <Col span={6}>{millify(exchange.price)}$</Col>
+                  </Row>
+                }
+              >
+                <h1>Hey</h1>
+                <p>This place is hardcoded section because api...</p>
+              </Panel>
+            </Collapse>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Exchanges;
